28d064f4ef731c166a4f3a7c19f763cd49dc5927
Developerzakir <zakir1234@!za>
2025-11-22 00:09:09 +0600

    Category crud Api created

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -13,6 +16,13 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $categories = Category::get();
+
+        return response()->json([
+            'status' => 'success',
+            'count' => count($categories),
+            'data' => $categories
+        ], 200);
     }
 
     /**
@@ -20,7 +30,27 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 'fail',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $data['name'] = $request->name;
+        $data['slug'] = Str::slug($request->name);
+
+        Category::create($data); // Create new record in databse table
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Category created successfully'
+        ], 201);
     }
 
     /**
@@ -36,7 +66,36 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 'fail',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $category = Category::find($id);
+
+        if($category){
+
+            $category->name = $request->name;
+            $category->slug = Str::slug($request->name);
+            $category->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Category editted successfully'
+            ], 201);
+
+        } else{
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Category Not Found'
+            ], 404);
+        }
     }
 
     /**
@@ -44,6 +103,21 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = Category::find($id);
+
+        if($category){
+
+            Category::destroy($id); // It will remove the category from our DB
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Category Deleted Successfully'
+            ], 201);
+        } else{
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Category Not Found'
+            ], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,4 +19,7 @@ Route::post('/login',[AuthController::class,'login'])->name('login');
 Route::group(['middleware'=>'auth:sanctum'], function(){
     Route::get('/profile',[AuthController::class,'profile'])->name('profile');
     Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+
+    // Blog Category Routes
+    Route::apiResource('categories', CategoryController::class);
 });
